b0ede5ed335a754c18390116f53d9ccff14a5467
Akas_Datta <012akasdatta@gmail.com>
2023-05-25 20:40:42 +0600

    added sectiontitle component. That use all components where i use

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,11 @@
+
+const SectionTitle = ({heading, subHeading}) => {
+    return (
+        <div className="text-center mx-auto md:w-4/12 my-10">
+            <p className="text-yellow-700 mb-4">{subHeading}</p>
+            <h3 className="text-black uppercase text-4xl border-y-4 py-3">{heading}</h3>
+        </div>
+    );
+};
+
+export default SectionTitle;
