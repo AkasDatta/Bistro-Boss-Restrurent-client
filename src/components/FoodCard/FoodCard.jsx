af3620bd59f666c00b1912356946cf69db72edba
Akas_Datta <012akasdatta@gmail.com>
2023-05-28 01:40:01 +0600

    modified foodcard and order componrnt

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -4,11 +4,12 @@ const FoodCard = ({item}) => {
     return (
         <div className="col">
             <div className="card h-full bg-gray-100 rounded-lg">
-            <img src={cardImage1} className="card-img-top" alt="Salad" />
+            <img src={image} className="card-img-top" alt="Salad" />
+            <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4 py-1 rounded">${price}</p>    
                 <div className="card-body flex flex-col justify-center items-center text-black">
-                    <h5 className="card-title">Caesar Salad</h5>
+                    <h5 className="card-title">{name}</h5>
                     <p className="card-text text-center">
-                        Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.
+                       {recipe}
                     </p>
                     <div className="card-actions">
                         <button className="btn btn-ghost text-yellow-600 border-b-4 px-6 border-yellow-700 hover:bg-black hover:text-yellow-600">
--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -4,9 +4,15 @@ import orderCoverImage from '../../../assets/shop/banner2.jpg';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import useMenu from '../../../Hooks/useMenu';
+import FoodCard from '../../../components/FoodCard/FoodCard';
 const Order = () => {
     const [tabIndex, setTabIndex] = useState(0);
     const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const salad = menu.filter(item => item.category === 'salad');
+    const offered = menu.filter(item => item.category === 'offered');
     return (
         <div>
             <Cover img={orderCoverImage} title="Order Food"></Cover>
@@ -18,7 +24,16 @@ const Order = () => {
                     <Tab>Dessert</Tab>
                     <Tab>Drinks</Tab>
                 </TabList>
-                <TabPanel></TabPanel>
+                <TabPanel>
+                   <div className='grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8 max-w-screen-xl mx-auto my-20'>
+                        {
+                            salad.map(item => <FoodCard
+                                key={item._id}
+                                item={item}
+                            ></FoodCard>)
+                        }
+                   </div>
+                </TabPanel>
                 <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
