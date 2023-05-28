9af28b0168fce4eeb4ccc7a882bb77fc33f74649
Akas_Datta <012akasdatta@gmail.com>
2023-05-28 19:04:50 +0600

    added menuitem and other file modified

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -6,12 +6,15 @@ import 'react-tabs/style/react-tabs.css';
 import useMenu from '../../../Hooks/useMenu';
 import OrderTab from '../OrderTab/OrderTab';
 import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 const Order = () => {
-    const [tabIndex, setTabIndex] = useState(0);
-    const [menu] = useMenu();
+    const categories = ['salad', 'pizza', 'soup','dessert', 'drinks' ];
     const {category} = useParams();
-    console.log(category)
+    const initialIndex = categories.indexOf(category);
+    const [tabIndex, setTabIndex] = useState(initialIndex);
+    const [menu] = useMenu();
+
     const desserts = menu.filter(item => item.category === 'dessert');
     const soup = menu.filter(item => item.category === 'soup');
     const pizza = menu.filter(item => item.category === 'pizza');
@@ -19,6 +22,9 @@ const Order = () => {
     const drinks = menu.filter(item => item.category === 'drinks');
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Order Food</title>
+            </Helmet>
             <Cover img={orderCoverImage} title="Order Food"></Cover>
             <Tabs className="text-center mt-24" defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
                 <TabList>
