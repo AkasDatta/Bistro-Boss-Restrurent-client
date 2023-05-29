98b3b29e151ece0fd7f4d0cdda1c1a4a1e4f74c1
Akas_Datta <012akasdatta@gmail.com>
2023-05-30 01:42:39 +0600

    added signup component and modified authprovider and other component

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,16 +1,17 @@
 import { Outlet, useLocation } from "react-router-dom";
-import Footer from "../Shared/Footer/Footer";
-import Navbar from "../Shared/Navbar/Navbar";
+import Navbar from '../../src/Shared/Navbar/Navbar.jsx';
+import Footer from '../../src/Shared/Footer/Footer.jsx';
 
 const Main = () => {
-    const location = useLocation()
-    console.log(location);
-    const noHeaderFooter = location.pathname.includes('login');
+    // const location = useLocation();
+    
+    const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup');
+
     return (
         <div>
-            {noHeaderFooter || <Navbar></Navbar>}
+            { noHeaderFooter || <Navbar></Navbar>}
             <Outlet></Outlet>
-            {noHeaderFooter || <Footer></Footer>}
+            { noHeaderFooter || <Footer></Footer>}
         </div>
     );
 };
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,54 @@
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
+export const authContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password); // Add 'auth' as the first argument
+      };
+      
+
+    const logOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect( () =>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            // console.log('Current User', currentUser);
+            setLoading(false);
+        });
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
+    }
+    return (
+        <authContext.Provider value={authInfo}>
+            {children}            
+        </authContext.Provider>
+    );
+};
+
+export default AuthProvider;
