3b599c4edd26ff0be25b5524038b6ba386efd9a1
Akas_Datta <012akasdatta@gmail.com>
2023-06-04 02:50:49 +0600

    modified hook components and others

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,4 +1,16 @@
+import { useContext } from 'react';
+import { useQuery } from 'react-query'
+import { authContext } from '../providers/AuthProvider';
 const useCart = email => {
+    const {user} = useContext(authContext);
 
+    const { refetch, data: cart = []} = useQuery({
+        queryKey: ['cart', user?.email],
+        queryFn: async () => {
+            const response = await fetch (`http://localhost:5000/carts?email=${user.email}`);
+            return response.json();
+        },
+    })
+    return [cart, refetch]
 }
 export default useCart;
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,21 @@ import './index.css'
 import { router } from './Routes/Router'
 import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
-import AuthProvider from './providers/AuthProvider'
+import AuthProvider from './providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>,
