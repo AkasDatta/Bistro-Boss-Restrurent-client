5322e621a5adaee92917cbed98a7392cf3d03e89
Akas_Datta <012akasdatta@gmail.com>
2023-06-04 01:07:30 +0600

    added useCart react hook component and install react query. also modified other file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,7 +5,8 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
+  // apiKey: import.meta.env.VITE_APIKEY,
+  apiKey: "AIzaSyAstx6VYIUMIYr5tABcxxcYO1GNTL88YKs",
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
