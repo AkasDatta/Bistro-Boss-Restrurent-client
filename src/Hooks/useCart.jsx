0c2963ede792ba29451af862268159fcb558a125
Akas_Datta <012akasdatta@gmail.com>
2023-06-06 17:16:17 +0600

    Make user api secure and verify Admin

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -2,11 +2,12 @@ import { useContext } from 'react';
 import { useQuery } from 'react-query'
 import { AuthContext } from '../providers/AuthProvider';
 const useCart = email => {
-    const {user} = useContext(AuthContext);
+    const {user, loading} = useContext(AuthContext);
     const token = localStorage.getItem('access-token');
 
     const { refetch, data: cart = []} = useQuery({
         queryKey: ['cart', user?.email],
+        enabled: !loading,
         queryFn: async () => {
             const response = await fetch (`http://localhost:5000/carts?email=${user.email}`, {headers: {
                 authorization: `bearer ${token}`
